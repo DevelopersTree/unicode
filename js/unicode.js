9011d9af6db6d4055d88a7ce435fc83b4ede7ee6
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-01-25 22:31:46 +0300

    add ability to redo test

--- js/unicode.js
+++ js/unicode.js
@@ -76,3 +76,49 @@ function colorizeSentence(userInput, characterResults) {
     console.log(colorized);
     return colorized;
 }
+
+function getTestReady(){
+    // Invoke check button when pressing enter inside textbox
+    $('#input').keypress(function (e) {
+      if (e.keyCode == 13)
+        $('#check').click();
+    });
+  
+    // Show a random proverb
+    $("#proverb").text(getRandomProverb());
+  
+    // Check keyboard
+    $("#check").click(function () {
+  
+      var userInput = $("#input").val();
+  
+      if (userInput.length == 0) {
+        alert('تکایە پەندەکە لە بۆکسەکە بنووسە');
+        return;
+      }
+  
+      if (userInput.length > 50) {
+        alert('پێویست ناکات زیاتر لە ٥٠ پیت بنووسیت');
+        return;
+      }
+  
+      var characterResults = getCharacterResults(userInput);
+      var isUsingStandardKeyboard = checkStandardKeyboard(characterResults);
+  
+      if (isUsingStandardKeyboard) {
+        $("#result").load('good.html', function() {
+          $("#again").click(function (){
+            $("#result").load("test.html", getTestReady);
+          })
+        });
+      } else {
+        $("#result").load('bad.html', function () {
+          var colorCoded = colorizeSentence(userInput, characterResults);
+          $("#colorized").html(colorCoded);
+          $("#again").click(function (){
+            $("#result").load("test.html", getTestReady);
+          })
+        });
+      }
+    });
+  }
